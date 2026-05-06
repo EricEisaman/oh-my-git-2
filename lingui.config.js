c7403a3a6a61de0368aaa84c31392e3d9e75a7db
FrameBuffer4096 <eric@framevr.io>
2026-05-06 19:58:46 -0400

    ESM fix

--- lingui.config.js
+++ lingui.config.js
@@ -1,8 +1,7 @@
 import {jstsExtractor, svelteExtractor} from "svelte-i18n-lingui/extractor"
-import languages from "./languages.ts"
 
 export default {
-    locales: languages,
+    locales: ["en", "de", "pt", "fr"],
     sourceLocale: "en",
     catalogs: [
         {
